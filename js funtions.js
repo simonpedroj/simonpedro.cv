397fdd658f791991eb28a675b4b28bb6c5fefca1
simonpedroj <126737301+simonpedroj@users.noreply.github.com>
2023-03-05 20:41:43 -0300

    Add files via upload

---
+++ js funtions.js
@@ -0,0 +1,11 @@
+document.getElementById ("boton") .onclick = function() {
+
+
+    console.log("capturamos el elemento clik");
+    document.getElementById("skills") .innerHTML = "Responsable,respetuoso,proactivo y puntual"
+}
+
+
+
+
+
